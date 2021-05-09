579d857b70b4afee5d3b348bb414f5da323874e1
Christian Hall <chall@intera.com>
2021-05-08 23:20:02 -0700

    Add material-ui and a navbar.

--- src/app.js
+++ src/app.js
@@ -1,29 +1,31 @@
 import logo from './logo.svg';
 import './app.css';
 
+import NavBar from './navbar';
+
+
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <Navbar />
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-
-    </div>
-  );
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
