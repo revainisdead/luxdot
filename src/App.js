eedf77954dd223cfb1e398e80b4f965df68eeb9e
Christian Hall <chall@intera.com>
2021-05-08 22:44:46 -0700

    Rename files.  - Add meta tag for author  - Change title to luxdot

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+
     </div>
   );
 }
